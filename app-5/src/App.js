a5d750626c79420a4aafc459d5d2a05d050f5df3
Ryan Ross <ryan@reross.com>
2022-03-08 12:21:38 -0600

    completed app-5

--- app-5/src/App.js
+++ app-5/src/App.js
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Image from "./Image";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Image
+        url={
+          "https://www.kindpng.com/picc/m/418-4189644_tottenham-hotspur-logo-jpg-hd-png-download.png"
+        }
+      />
     </div>
   );
 }
