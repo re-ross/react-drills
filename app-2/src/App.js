6c8845af6c1bcd435e9b4db9f8ac98b404ab42a4
Ryan Ross <ryan@reross.com>
2022-03-08 11:29:23 -0600

    solved app-2

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,26 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const food = ["spaghetti", "ice cream", "sushi", "bologna", "cheese"];
+  const list = food.map((item) => <h2>{item}</h2>);
+  return <div className="App">{list}</div>;
 }
 
 export default App;
